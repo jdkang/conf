11eeb59317d7b7df0f2e9b08c67d2d8093350cc0
Jai Kang <1807929+jdkang@users.noreply.github.com>
2021-01-30 18:25:34 -0500

    init

---
+++ linux/copy_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cp $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+cp $SCRIPT_DIR/.vimrc ~/.vimrc
+
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+cat <<EOF >> ~/.bashrc
+# git prompt
+if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source $HOME/.bash-git-prompt/gitprompt.sh
+fi
+EOF
