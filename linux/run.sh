e8c918baa8b73cdd1797bb2fd18360df9be2158c
Jai Kang <1807929+jdkang@users.noreply.github.com>
2021-12-31 20:52:53 -0500

    add p10k.zsh conf

--- linux/run.sh
+++ linux/run.sh
@@ -2,12 +2,20 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # oh my zsh
-sh -c "$(wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [ ! -d ~/.oh-my-zsh  
+then
+    sh -c "$(wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 # powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]
+then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 
 # conf files
 cp $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
 cp $SCRIPT_DIR/.vimrc ~/.vimrc
 cp $SCRIPT_DIR/.zshrc ~/.zshrc
 
+# reload zsh
+exec zsh
