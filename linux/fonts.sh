e314dac34efd314df175e952ff2f8eca1d603c66
Jai Kang <1807929+jdkang@users.noreply.github.com>
2022-01-02 21:51:50 -0500

    add linux nerd font install

---
+++ linux/fonts.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+fontDir="$HOME/.local/share/fonts"
+tmpDir=`mktemp -d`
+mkdir $tmpDir/ttf
+
+# firacode NF
+echo "checking firacode nf"
+if ! grep -qi "FiraCode NF" <(fc-list) ; then
+    echo "downloading latest zip"
+    curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
+    | grep -i "browser_download_url.*FiraCode\.zip" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -q -i - -O $tmpDir/FiraCode.zip
+
+    echo "unpacking"
+    unzip -q $tmpDir/FiraCode.zip -d $tmpDir/ttf
+else
+    echo "  already installed"
+fi
+
+# install fonts
+if [ $(find $tmpDir/ttf -maxdepth 1 -name '*.ttf' | wc -l) != '0' ] ; then
+    if [ ! -d $fontDir ] ; then
+        echo "creating local font dir"
+        mkdir -p $fontDir
+    fi
+    
+    echo "copying fonts"
+    cp $tmpDir/ttf/*.ttf $fontDir
+
+    echo "refreshing font cache"
+    fc-cache -f
+else
+    echo "nothing to install"
+fi
+
+
