b0e3eb9851f08f6812d3835701936257279d6d76
Jai Kang <1807929+jdkang@users.noreply.github.com>
2022-01-06 09:36:23 -0500

    windows font installer

---
+++ windows/fonts.ps1
@@ -0,0 +1,107 @@
+param(
+    [Parameter(Mandatory=$False)]
+    [regex]
+    $FontAssetRegex = 'FiraCode\.zip$'
+)
+
+# ----------------------------------------------
+# func
+# ----------------------------------------------
+function Get-LatestGithubArtifact {
+param(
+    [Parameter(Mandatory=$True)]
+    [ValidateScript({ $_ -match '\w+\/\w+' })]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $OrgRepo,
+
+    [regex]
+    $AssetRegex
+)
+    $ghLatestReleaseFmt = 'https://api.github.com/repos/{0}/releases/latest'
+    $ghLatestUrl = $ghLatestReleaseFmt -f $OrgRepo
+    $ghLatestReleaseResult = Invoke-RestMethod -Uri $ghLatestUrl
+    $assets = @($ghLatestReleaseResult.assets)
+    if($assets.Count -gt 0) {
+        $asset = $assets |
+                    sort-object -property 'id' -descending |
+                    where-object { $_.name -match $AssetRegex } |
+                    select-object -first 1
+        if($asset.browser_download_url) {
+            return $asset
+        } else {
+            throw "cannot find assetg matching regex: $($AssetRegex)"
+        }
+    } else {
+        throw "cannot find latest gh release for: $($OrgRepo)"
+    }
+}
+function Install-Fonts {
+param(
+    [Parameter(Mandatory=$True)]
+    [ValidateScript({ Test-Path -Path $_ -PathTyupe 'container' })]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $FolderPath,
+
+    [regex]
+    $FileNameRegex = '\.ttf$'
+)
+    $installFontList = @()
+    $installedFontDir = "$($ENV:LocalAppData)\Microsoft\Windows\Fonts"
+    $filteredFilesList = @(Get-ChildItem -File -Path $FolderPath | Where-Object { $_.Name -match $FileNameRegex })
+    if($filteredFilesList.Count -eq 0) {
+        write-warning "No fonts matching regex '$($FileNameRegex)'' were found"
+        return
+    }
+    $installedFontFileNames = @(Get-ChildItem -File -Path $installedFontDir | Select-Object -ExpandProperty 'name')
+    foreach($fontFile in $filteredFilesList) {
+        $cleanedName = $fontFile.Name -replace '_+',''
+        if((($installedFontFileNames -notcontains $cleanedName)) -and
+           (($installedFontFileNames -notcontains $fontFile.Name)))
+        {
+            $installFontList += $fontFile
+        }
+        else {
+            write-host "font already installed: $($fontFile.Name)" -f yellow
+        }
+    }
+    if($installFontList.Count -gt 0) {
+        try {
+            $objShell = New-Object -ComObject Shell.Application
+            $objFolder = $objShell.Namespace(0x14)
+            foreach($installFont in $installFontList) {
+                write-host "installing font: $($installFont.Name)" -f cyan
+                $objFolder.CopyHere($installFont.fullname)
+            }
+        }
+        catch {
+            write-warning $_.Exception.Message
+        }
+    }
+}
+
+# ----------------------------------------------
+# main
+# ----------------------------------------------
+$tempFolder = [System.IO.Path]::GetTempPath()
+$tempFontFolder = Join-Path $tempFolder "FiraCodeNF_$(get-date -f 'yyyyMMddHHmmss')"
+
+# download FiraCode nerd font
+$latestFiraCodeNfAsset = Get-LatestGithubArtifact -OrgRepo 'ryanoasis/nerd-fonts' -AssetRegex $FontAssetRegex
+if($latestFiraCodeNfAsset.browser_download_url) {
+    $tempFile = Join-Path $tempFolder 'FiraCodeNF.zip'
+    write-host "downloading FiraCode.zip $($latestFiraCodeNfAsset.browser_download_url)" -f green
+    Invoke-RestMethod -Uri $latestFiraCodeNfAsset.browser_download_url -OutFile $tempFile
+    write-host "unpacking" -f green
+    mkdir $tempFontFolder -ea 0 -force | out-null
+    Expand-Archive -Path $tempFile -DestinationPath $tempFontFolder -Force
+} else {
+    write-warning "cannot locate cmder latest assets: $($ompGhLatestReleaseUri)"
+}
+
+# install fonts
+if(Test-Path -Path $tempFontFolder -PathType 'container') {
+    Install-Fonts -FolderPath $tempFontFolder
+}
+
