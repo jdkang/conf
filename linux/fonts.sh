3b839da3935753cc27259af2a8b8c83517da26f7
Jai Kang <1807929+jdkang@users.noreply.github.com>
2022-01-06 19:27:53 -0500

    font.sh takes param

--- linux/fonts.sh
+++ linux/fonts.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
-fontDir="$HOME/.local/share/fonts"
-tmpDir=`mktemp -d`
-mkdir $tmpDir/ttf
 
+font_zip_name="${1:-FiraCode}"
+tmpDir=`mktemp -d`
+tempFontDir="${tmpDir}/fonts"
+
+userFontDir="$HOME/.local/share/fonts"
+if [[ $(uname) == 'Darwin' ]]; then
+    userFontDir="$HOME/Library/Fonts"
+fi
+
+# ensure font cache command
 if ! command -v fc-list &>/dev/null ; then
     >&2 echo "fc-list not found"
     exit 1
 fi
 
-# firacode NF
-echo "checking firacode nf"
-if ! grep -qi "FiraCode NF" <(fc-list) ; then
-    echo "downloading latest zip"
-    curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-    | grep -i "browser_download_url.*FiraCode\.zip" \
-    | cut -d : -f 2,3 \
-    | tr -d \" \
-    | wget -q -i - -O $tmpDir/FiraCode.zip
+# temp dir
+mkdir $tempFontDir
 
-    echo "unpacking"
-    unzip -q $tmpDir/FiraCode.zip -d $tmpDir/ttf
-else
-    echo "  already installed"
+# download font
+download_url=""
+zip_regex="browser_download_url.*${font_zip_name}\.zip"
+latestAsset=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest)
+assetMatch=$(grep -Ei $zip_regex <<< $latestAsset)
+local_zip_file="${tmpDir}/${font_zip_name}.zip"
+if [[ ! -z $assetMatch ]] ; then
+    download_url=$( echo $assetMatch | cut -d : -f 2,3 | tr -d \" )
+    echo "downloading ${download_url}"
+    wget -q -O $local_zip_file $download_url
+    echo "unpacking ${local_zip_file}"
+    unzip -q $local_zip_file -d $tempFontDir
 fi
 
 # install fonts
-if [ $(find $tmpDir/ttf -maxdepth 1 -name '*.ttf' | wc -l) != '0' ] ; then
-    if [ ! -d $fontDir ] ; then
-        echo "creating local font dir"
-        mkdir -p $fontDir
+if [[ $(find $tempFontDir -maxdepth 1 -name '*.ttf' | wc -l) != '0' ]] ; then
+    if [ ! -d $userFontDir ] ; then
+        echo "creating local font dir ${userFontDir}"
+        mkdir -p $userFontDir
     fi
     
-    echo "copying fonts"
-    cp $tmpDir/ttf/*.ttf $fontDir
+    echo "copying fonts to ${userFontDir}"
+    cp -f $tempFontDir/*.ttf $userFontDir
 
     echo "refreshing font cache"
     fc-cache -f
@@ -41,4 +49,8 @@ else
     echo "nothing to install"
 fi
 
-
+# cleanup
+if [[ -d $tmpDir ]] ; then
+    echo "cleaning up temp dir ${tmpDir}"
+    rm -rf $tmpDir
+fi
